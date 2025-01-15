b7489b99267499057a2e2a55a2ed0b972bcf0e06
MXpenguin <r.max.beesley@gmail.com>
2025-01-15 09:49:53 -0700

    added framework for checking bishop moves and added implementation of methods in ChessMove class

--- shared/src/main/java/chess/BishopMovesCalculator.java
+++ shared/src/main/java/chess/BishopMovesCalculator.java
@@ -7,8 +7,26 @@ public class BishopMovesCalculator implements PieceMovesCalculator {
     public Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition position) {
         Collection<ChessMove> moves = new ArrayList<ChessMove>();
 
-        for (int i=1; i<=8; ++i) {
-            moves.add();
+        int currentRow = position.getRow();
+        int currentCol = position.getColumn();
+
+        for (int i = 1; i <= 8; ++i) {
+            if (currentRow + i <= 8 && currentCol + i <= 8) {
+                ChessPosition endPosition = new ChessPosition(currentRow+i, currentCol+i);
+                moves.add(new ChessMove(position, endPosition, ChessPiece.PieceType.BISHOP));
+            }
+            if (currentRow - i >= 1 && currentCol - i >= 1) {
+                ChessPosition endPosition = new ChessPosition(currentRow-i, currentCol-i);
+                moves.add(new ChessMove(position, endPosition, ChessPiece.PieceType.BISHOP));
+            }
+            if (currentRow + i <= 8 && currentCol - i >= 1) {
+                ChessPosition endPosition = new ChessPosition(currentRow+i, currentCol-i);
+                moves.add(new ChessMove(position, endPosition, ChessPiece.PieceType.BISHOP));
+            }
+            if (currentRow - i >= 1 && currentCol + i <= 8) {
+                ChessPosition endPosition = new ChessPosition(currentRow-i, currentCol+i);
+                moves.add(new ChessMove(position, endPosition, ChessPiece.PieceType.BISHOP));
+            }
         }
 
         return moves;
