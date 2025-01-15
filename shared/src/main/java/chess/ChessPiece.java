38751280eb3cfe5bd3faa491ac5d54d7a142f156
MXpenguin <r.max.beesley@gmail.com>
2025-01-15 12:23:42 -0700

    overridden equals and hashcode methods for ChessMove, ChessPiece, ChessPosition classes

--- shared/src/main/java/chess/ChessPiece.java
+++ shared/src/main/java/chess/ChessPiece.java
@@ -1,6 +1,7 @@
 package chess;
 
 import java.util.Collection;
+import java.util.Objects;
 
 /**
  * Represents a single chess piece
@@ -67,5 +68,19 @@ public class ChessPiece {
 //            case PAWN -> {}
             default -> throw new RuntimeException("No type");
         };
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        ChessPiece that = (ChessPiece) o;
+        return pieceColor == that.pieceColor && type == that.type;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(pieceColor, type);
     }
 }
