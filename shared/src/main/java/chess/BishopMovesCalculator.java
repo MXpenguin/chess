957d2136d2419f4ba77a2660062182bab359a704
MXpenguin <r.max.beesley@gmail.com>
2025-01-14 22:17:22 -0700

    added framework for checking bishop moves and added implementation of methods in ChessMove class

--- shared/src/main/java/chess/BishopMovesCalculator.java
+++ shared/src/main/java/chess/BishopMovesCalculator.java
@@ -1,9 +1,16 @@
 package chess;
 
+import java.util.ArrayList;
 import java.util.Collection;
 
 public class BishopMovesCalculator implements PieceMovesCalculator {
     public Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition position) {
-        throw new RuntimeException("Not implemented");
+        Collection<ChessMove> moves = new ArrayList<ChessMove>();
+
+        for (int i=1; i<=8; ++i) {
+            moves.add();
+        }
+
+        return moves;
     };
 }
