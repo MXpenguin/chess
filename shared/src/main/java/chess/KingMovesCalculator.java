337880f15b4b116e454a591936fa8f8a3969e904
MXpenguin <r.max.beesley@gmail.com>
2025-01-14 21:57:51 -0700

    added piece move calculator class interface and two subclasses for king and bishop

---
+++ shared/src/main/java/chess/KingMovesCalculator.java
@@ -0,0 +1,9 @@
+package chess;
+
+import java.util.Collection;
+
+public class KingMovesCalculator implements PieceMovesCalculator {
+    public Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition position) {
+        throw new RuntimeException("Not implemented");
+    };
+}
