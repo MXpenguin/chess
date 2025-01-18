29e8b2fbac32427fd670d22eb88ef9719742f6ed
MXpenguin <r.max.beesley@gmail.com>
2025-01-18 10:10:51 -0700

    Passed bishop moves tests

--- shared/src/main/java/chess/BishopMovesCalculator.java
+++ shared/src/main/java/chess/BishopMovesCalculator.java
@@ -9,33 +9,86 @@ public class BishopMovesCalculator implements PieceMovesCalculator {
 
         int currentRow = position.getRow();
         int currentCol = position.getColumn();
-
-        for (int i = 1; i <= 8; ++i) {
-//            if (currentRow + i <= 8 && currentCol + i <= 8) {
-//                ChessPosition endPosition = new ChessPosition(currentRow+i, currentCol+i);
-//                moves.add(new ChessMove(position, endPosition, null));
-//            }
-            if (currentRow - i >= 1 && currentCol - i >= 1) {
-                ChessPosition endPosition = new ChessPosition(currentRow-i, currentCol-i);
-                moves.add(new ChessMove(position, endPosition, null));
-            }
-            if (currentRow + i <= 8 && currentCol - i >= 1) {
-                ChessPosition endPosition = new ChessPosition(currentRow+i, currentCol-i);
-                moves.add(new ChessMove(position, endPosition, null));
-            }
-            if (currentRow - i >= 1 && currentCol + i <= 8) {
-                ChessPosition endPosition = new ChessPosition(currentRow-i, currentCol+i);
-                moves.add(new ChessMove(position, endPosition, null));
-            }
-        }
-
-        // upper diagonal moves
+        
+        // upper right diagonal moves
         for (int i = 1; i < 8; ++i) {
             int endRow = currentRow + i;
             int endCol = currentCol + i;
 
             // Check if out of bounds
             if (endRow > 8 || endCol > 8) {
+                break;
+            }
+
+            ChessPosition endPosition = new ChessPosition(endRow, endCol);
+
+            // Check if end square blocked
+            ChessPiece endPositionPiece = board.getPiece(endPosition);
+            if (endPositionPiece != null) {
+                if (endPositionPiece.getTeamColor() != board.getPiece(position).getTeamColor()) {
+                    moves.add(new ChessMove(position, endPosition, null));
+                }
+                break;
+            }
+
+            moves.add(new ChessMove(position, endPosition, null));
+        }
+
+        // upper left diagonal moves
+        for (int i = 1; i < 8; ++i) {
+            int endRow = currentRow + i;
+            int endCol = currentCol - i;
+
+            // Check if out of bounds
+            if (endRow > 8 || endCol < 1) {
+                break;
+            }
+
+            ChessPosition endPosition = new ChessPosition(endRow, endCol);
+
+            // Check if end square blocked
+            ChessPiece endPositionPiece = board.getPiece(endPosition);
+            if (endPositionPiece != null) {
+                if (endPositionPiece.getTeamColor() != board.getPiece(position).getTeamColor()) {
+                    moves.add(new ChessMove(position, endPosition, null));
+                }
+                break;
+            }
+
+            moves.add(new ChessMove(position, endPosition, null));
+        }
+
+        // lower right diagonal moves
+        for (int i = 1; i < 8; ++i) {
+            int endRow = currentRow - i;
+            int endCol = currentCol + i;
+
+            // Check if out of bounds
+            if (endRow < 1 || endCol > 8) {
+                break;
+            }
+
+            ChessPosition endPosition = new ChessPosition(endRow, endCol);
+
+            // Check if end square blocked
+            ChessPiece endPositionPiece = board.getPiece(endPosition);
+            if (endPositionPiece != null) {
+                if (endPositionPiece.getTeamColor() != board.getPiece(position).getTeamColor()) {
+                    moves.add(new ChessMove(position, endPosition, null));
+                }
+                break;
+            }
+
+            moves.add(new ChessMove(position, endPosition, null));
+        }
+
+        // lower left diagonal moves
+        for (int i = 1; i < 8; ++i) {
+            int endRow = currentRow - i;
+            int endCol = currentCol - i;
+
+            // Check if out of bounds
+            if (endRow < 1 || endCol < 1) {
                 break;
             }
 
