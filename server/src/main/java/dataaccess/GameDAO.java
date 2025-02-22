a1d9640e77ec73937e5af928a46d34ef2692f7ad
MXpenguin <r.max.beesley@gmail.com>
2025-02-22 11:09:25 -0700

    Added most of the methods for the data access interfaces

--- server/src/main/java/dataaccess/GameDAO.java
+++ server/src/main/java/dataaccess/GameDAO.java
@@ -1,7 +1,15 @@
 package dataaccess;
 
+import model.GameData;
+
+import java.util.Collection;
+
 public interface GameDAO {
 
     void clear();
 
+    void createGame(GameData gameData);
+    GameData getGame(String gameID);
+    Collection<GameData> listGames();
+    void updateGame(String gameID, String update);
 }
