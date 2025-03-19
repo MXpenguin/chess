2b0cc8e55772e55297ac05b4e778c39552cc05d2
MXpenguin <r.max.beesley@gmail.com>
2025-03-19 17:03:40 -0600

    Added stubs for all endpoint methods in ServerFacade

--- shared/src/main/java/server/ServerFacade.java
+++ shared/src/main/java/server/ServerFacade.java
@@ -1,9 +1,35 @@
 package server;
 
+import resultsandrequests.*;
+
 public class ServerFacade {
     private final String serverUrl;
 
     public ServerFacade(String url) {
         serverUrl = url;
+    }
+
+    public RegisterResult register(RegisterRequest request) {
+        return null;
+    }
+
+    public LoginResult login(LoginRequest request) {
+        return null;
+    }
+
+    public LogoutResult logout(LogoutRequest request) {
+        return null;
+    }
+
+    public ListGamesResult listGames(ListGamesRequest request) {
+        return null;
+    }
+
+    public CreateGameResult createGame(CreateGameRequest request) {
+        return null;
+    }
+
+    public JoinGameResult joinGame(JoinGameRequest request) {
+        return null;
     }
 }
