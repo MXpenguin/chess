d3d4ca8fcc5b596e35dceb00054c22d92e7bd851
MXpenguin <r.max.beesley@gmail.com>
2025-03-20 16:39:47 -0600

    Added ResponseException class

--- shared/src/main/java/server/ServerFacade.java
+++ shared/src/main/java/server/ServerFacade.java
@@ -1,6 +1,14 @@
 package server;
 
+import com.google.gson.Gson;
 import resultsandrequests.*;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URI;
+import java.net.URL;
 
 public class ServerFacade {
     private final String serverUrl;
@@ -10,6 +18,9 @@ public class ServerFacade {
     }
 
     public RegisterResult register(RegisterRequest request) {
+        String path = "/user";
+        String method = "POST";
+        String body = toJson(request);
         return null;
     }
 
@@ -31,5 +42,53 @@ public class ServerFacade {
 
     public JoinGameResult joinGame(JoinGameRequest request) {
         return null;
+    }
+
+    private <T> T makeRequest(String path, String method, String authToken, String body, Class<T> resultClass) {
+        try {
+            URL url = (new URI(serverUrl + path)).toURL();
+            HttpURLConnection http = (HttpURLConnection) url.openConnection();
+            http.setRequestMethod(method);
+            http.setDoOutput(true);
+
+            if (authToken != null) {
+                http.addRequestProperty("authorization", authToken);
+            }
+
+            try (OutputStream reqBody = http.getOutputStream()) {
+                reqBody.write(body.getBytes());
+            }
+
+            http.connect();
+
+            throwIfNotSuccessful(http);
+
+            return readBody(http, responseClass);
+        } catch (ResponseException ex) {
+            throw ex;
+        } catch (Exception ex) {
+            throw new ResponseException(500, ex.getMessage());
+        }
+    }
+
+    private void throwIfNotSuccessful(HttpURLConnection http) throws IOException, ResponseException {
+        var status = http.getResponseCode();
+        if (!isSuccessful(status)) {
+            try (InputStream respErr = http.getErrorStream()) {
+                if (respErr != null) {
+                    throw ResponseException.fromJson(respErr);
+                }
+            }
+
+            throw new ResponseException(status, "other failure: " + status);
+        }
+    }
+
+    private String toJson(Object object) {
+        return new Gson().toJson(object);
+    }
+
+    private boolean isSuccessful(int status) {
+        return 200 <= status && status < 300;
     }
 }
