a87fbec43863d20003a4ed60c763f826b50696ec
MXpenguin <r.max.beesley@gmail.com>
2025-03-22 19:09:46 -0600

    Working on prelogin ui

--- client/src/main/java/client/PostLoginClient.java
+++ client/src/main/java/client/PostLoginClient.java
@@ -1,6 +1,21 @@
 package client;
 
+import server.ServerFacade;
+
 public class PostLoginClient implements Client {
+    private final ServerFacade server;
+    private final String serverUrl;
+    private final String username;
+    private final String authToken;
+
+    public PostLoginClient(String serverUrl, String username, String authToken) {
+        server = new ServerFacade(serverUrl);
+        this.serverUrl = serverUrl;
+        this.username = username;
+        this.authToken = authToken;
+    }
+
+
     @Override
     public String eval(String eval) {
         return "";
--- client/src/main/java/client/PreLoginClient.java
+++ client/src/main/java/client/PreLoginClient.java
@@ -1,6 +1,11 @@
 package client;
 
+import resultsandrequests.RegisterRequest;
+import resultsandrequests.RegisterResult;
+import server.ResponseException;
 import server.ServerFacade;
+
+import java.util.Arrays;
 
 public class PreLoginClient implements Client {
 
@@ -13,9 +18,20 @@ public class PreLoginClient implements Client {
     }
 
     @Override
-    public String eval(String eval) {
-
-        return "";
+    public String eval(String input) {
+        try {
+            var tokens = input.toLowerCase().split(" ");
+            var cmd = (tokens.length > 0) ? tokens[0] : "help";
+            var params = Arrays.copyOfRange(tokens, 1, tokens.length);
+            return switch (cmd) {
+                case "register" -> register(params);
+                case "login" -> login(params);
+                case "quit" -> "quit";
+                default -> help();
+            };
+        } catch (ResponseException ex) {
+            return ex.getMessage();
+        }
     }
 
     @Override
@@ -26,5 +42,20 @@ public class PreLoginClient implements Client {
                 quit
                 help
                 """;
+    }
+
+    private String register(String... params) throws ResponseException {
+        if (params.length != 3) {
+            return "Please provide a username, password, and email.";
+        }
+        RegisterResult result = server.register(new RegisterRequest(params[0], params[1], params[2]));
+        String username = result.getUsername();
+        String authToken = result.getAuthToken();
+        new Repl(new PostLoginClient(serverUrl, username, authToken)).run();
+        return "";
+    }
+
+    private String login(String... params) throws ResponseException {
+        return "";
     }
 }
