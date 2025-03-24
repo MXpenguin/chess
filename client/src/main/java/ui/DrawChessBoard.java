42360258d27312c8bfaed923ef4d142de8e7a848
MXpenguin <r.max.beesley@gmail.com>
2025-03-24 13:31:05 -0600

    DrawChessBoard draws the chessboard nicely. Need to fix column labels

--- client/src/main/java/ui/DrawChessBoard.java
+++ client/src/main/java/ui/DrawChessBoard.java
@@ -21,8 +21,8 @@ public class DrawChessBoard {
         this.game = gameData.game();
         this.board = game.getBoard();
 
-        this.DARK_SQUARE_COLOR = SET_BG_COLOR_DARK_GREY;
-        this.LIGHT_SQUARE_COLOR = SET_BG_COLOR_LIGHT_GREY;
+        this.DARK_SQUARE_COLOR = SET_BG_COLOR_DARK_GREY + SET_TEXT_COLOR_DARK_GREY;
+        this.LIGHT_SQUARE_COLOR = SET_BG_COLOR_LIGHT_GREY + SET_TEXT_COLOR_LIGHT_GREY;
         this.DARK_PIECE_COLOR = SET_TEXT_COLOR_BLACK;
         this.LIGHT_PIECE_COLOR = SET_TEXT_COLOR_WHITE;
     }
@@ -31,19 +31,9 @@ public class DrawChessBoard {
         StringBuilder boardBuilder = new StringBuilder();
         boardBuilder.append(SET_TEXT_COLOR_WHITE).append(" a b c d e f g h \n");
         for (int row = 8; row >= 1; --row) {
-            boardBuilder.append(SET_TEXT_COLOR_WHITE).append(intToChar(row)).append(" ");
+            boardBuilder.append(SET_TEXT_COLOR_WHITE).append(row).append(" ");
             for (int col = 1; col <= 8; ++col) {
-                // black or white square
-                boardBuilder.append((row+col) % 2 == 0 ? DARK_SQUARE_COLOR : LIGHT_SQUARE_COLOR);
-
-                ChessPiece piece = board.getPiece(new ChessPosition(row, col));
-                if (piece != null) {
-                    boardBuilder.append(piece.getTeamColor() == ChessGame.TeamColor.WHITE
-                            ? LIGHT_PIECE_COLOR : DARK_PIECE_COLOR);
-                    boardBuilder.append(piece);
-                } else {
-                    boardBuilder.append(EMPTY);
-                }
+                boardBuilder.append(drawSquare(row, col));
             }
             boardBuilder.append(RESET_BG_COLOR).append(SET_TEXT_COLOR_WHITE).append(" ").append(intToChar(row));
             boardBuilder.append("\n");
@@ -56,6 +46,24 @@ public class DrawChessBoard {
     public String drawBlackPerspective() {
 
         return "black perspective";//TODO
+    }
+
+    private String drawSquare(int row, int col) {
+        StringBuilder squareBuilder = new StringBuilder();
+
+        //black or white
+        squareBuilder.append((row+col) % 2 == 0 ? DARK_SQUARE_COLOR : LIGHT_SQUARE_COLOR);
+
+        ChessPiece piece = board.getPiece(new ChessPosition(row, col));
+        if (piece != null) {
+            squareBuilder.append(piece.getTeamColor() == ChessGame.TeamColor.WHITE
+                    ? LIGHT_PIECE_COLOR : DARK_PIECE_COLOR);
+            squareBuilder.append(piece);
+        } else {
+            squareBuilder.append(BLACK_PAWN);//Empty square
+        }
+
+        return squareBuilder.toString();
     }
 
     private String intToChar(int integer) {
